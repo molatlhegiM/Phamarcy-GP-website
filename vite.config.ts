bcef1bb8f079845c834027fff8923c2bab47f3c1
molatlhegiM <139362696+molatlhegiM@users.noreply.github.com>
2026-02-05 16:03:03 +0200

    Fix deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => ({
-  base: './',
+  base: '/Phamarcy-GP-website/',
 
   server: {
     port: 3000,
